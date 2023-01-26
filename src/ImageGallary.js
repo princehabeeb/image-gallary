4ce4018e80452aaca4777f5e6f7e7ba10dfe6f29
princehabeeb <habiy3400@gmail.com>
2023-01-26 01:08:19 +0100

    new

--- src/ImageGallary.js
+++ src/ImageGallary.js
@@ -3,8 +3,37 @@ import React, { Component } from 'react'
 export default class ImageGallary extends Component {
   
   render() {
+    let imgGallary = [
+        {
+            src: './Images/img1.jpg',
+            title: 'img1'
+        },
+        {
+            src: './Images/img2.jpg',
+            title: 'img2'
+        },
+        {
+            src: './Images/img3.jpg',
+            title: 'img3'
+        },
+        {
+            src: './Images/img4.jpg',
+            title: 'img4'
+        },
+        {
+            src: './Images/img5.jpg',
+            title: 'img5'
+        },
+        {
+            src: './Images/img6.jpg',
+            title: 'img6'
+        }
+    ];
     return (
-      <div>ImageGallary</div>
+      <div>
+        <h1>Image Gallary</h1>
+        <hr />
+    </div>
     )
   }
 }
